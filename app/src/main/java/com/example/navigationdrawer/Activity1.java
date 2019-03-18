be6340e561472fe4aa1b3954e327b6b5b87494fc
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2019-03-18 23:38:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawer/Activity1.java
@@ -0,0 +1,24 @@
+package com.example.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Activity1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Menu 2");
+    }
+}
